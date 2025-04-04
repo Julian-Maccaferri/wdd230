5c8f8e1861e7d687cd400e2b9d47e87394bee859
Julian-Maccaferri <tulimaccaferri@gmail.com>
2025-04-04 20:27:37 -0300

    Chamber discover page ready

---
+++ chamber/scripts/visitMessage.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const visitMessageElement = document.getElementById("visitMessage");
+    const lastVisit = localStorage.getItem("lastVisit");
+    const currentVisit = new Date().getTime();
+
+    if (!lastVisit) {
+        // First visit
+        visitMessageElement.textContent = "¡Welcome! if you have any questions or need help, please let me know!";    } else {
+        const lastVisitTime = parseInt(lastVisit, 10);
+        const timeDifference = currentVisit - lastVisitTime;
+        const daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+        if (daysDifference < 1) {
+            visitMessageElement.textContent = "¡Welcome Back!";
+        } else if (daysDifference === 1) {
+            visitMessageElement.textContent = "Your last visit was 1 day ago.";
+        } else {
+            visitMessageElement.textContent = `Your last visit was ${daysDifference} days ago.`;
+        }
+    }
+
+    // Update the last visit time in localStorage
+    localStorage.setItem("lastVisit", currentVisit.toString());
+});
