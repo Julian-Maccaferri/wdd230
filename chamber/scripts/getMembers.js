2996d0477cd85e6249502fa9635745ab4c65f86c
Julian-Maccaferri <tulimaccaferri@gmail.com>
2025-04-14 19:52:31 -0300

    Chamber directory page advance

---
+++ chamber/scripts/getMembers.js
@@ -0,0 +1,41 @@
+const membersURL = 'data/members.json';
+const cards = document.querySelector('#cards');
+
+async function getJsonMembers() {
+    const response = await fetch(membersURL);
+    const data = await response.json();
+    console.log(data); // For test only
+    displayMembers(data.members);
+}
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        let card = document.createElement('section');
+        let name = document.createElement('h2');
+        let address = document.createElement('p');
+        let website = document.createElement('p');
+        let img = document.createElement('img');
+
+        name.textContent = `Company Name: ${member.name}`;
+        address.textContent = `Address: ${member.address}`;
+        website.textContent = `Url: ${member.website}`;
+
+        // Set the image attributes
+        img.setAttribute('src', member.img);
+        img.setAttribute('alt', `${member.name} logo`);
+        img.setAttribute('loading', 'lazy');
+        img.setAttribute('width', '300px');
+        img.setAttribute('height', '200px');
+
+        card.appendChild(name);
+        card.appendChild(address)
+        card.appendChild(website);
+        card.appendChild(img);
+
+        cards.appendChild(card);
+    });
+}
+
+getJsonMembers();
+
+
