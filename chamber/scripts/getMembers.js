978c51e75e6d60f16fbf731d600e2fd845450cd0
Julian-Maccaferri <tulimaccaferri@gmail.com>
2025-04-14 22:38:04 -0300

    Chamber Directory Page Ready

--- chamber/scripts/getMembers.js
+++ chamber/scripts/getMembers.js
@@ -11,26 +11,33 @@ async function getJsonMembers() {
 const displayMembers = (members) => {
     members.forEach((member) => {
         let card = document.createElement('section');
+        card.setAttribute('class', 'card');
         let name = document.createElement('h2');
         let address = document.createElement('p');
+        let phone = document.createElement('p');
         let website = document.createElement('p');
+        let subscription = document.createElement('p');
         let img = document.createElement('img');
 
-        name.textContent = `Company Name: ${member.name}`;
+        name.textContent = `${member.name}`;
         address.textContent = `Address: ${member.address}`;
-        website.textContent = `Url: ${member.website}`;
+        phone.textContent = `Phone: ${member.phone}`;
+        subscription.innerHTML = `<strong>Subscription: ${member.subscription}</strong>`;
+        website.textContent = `${member.website}`;
 
         // Set the image attributes
         img.setAttribute('src', member.img);
         img.setAttribute('alt', `${member.name} logo`);
         img.setAttribute('loading', 'lazy');
-        img.setAttribute('width', '300px');
-        img.setAttribute('height', '200px');
+        img.setAttribute('width', 'auto');
+        img.setAttribute('height', '50px');
 
         card.appendChild(name);
         card.appendChild(address)
+        card.appendChild(phone);
         card.appendChild(website);
         card.appendChild(img);
+        card.appendChild(subscription);
 
         cards.appendChild(card);
     });
